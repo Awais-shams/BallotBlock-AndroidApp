5ae85a3350bc33714a315a5225cd4faa76c54b5c
abd-ashraf <abdaman7x@gmail.com>
2021-12-28 13:28:59 +0500

    commit 2

--- app/src/main/java/com/example/ballotblock/Authentication/Register1.java
+++ app/src/main/java/com/example/ballotblock/Authentication/Register1.java
@@ -3,15 +3,19 @@ package com.example.ballotblock.Authentication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.example.ballotblock.R;
 
 public class Register1 extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register1);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar = findViewById(R.id.myToolbar);
+        getSupportActionBar();
     }
 }
