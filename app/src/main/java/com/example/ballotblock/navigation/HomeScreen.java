d6e5cd1afda842058a616fcb447d9cdf8ee145a5
abd-ashraf <abdaman7x@gmail.com>
2022-01-20 22:28:07 +0500

    Huzaifa Work

---
+++ app/src/main/java/com/example/ballotblock/navigation/HomeScreen.java
@@ -0,0 +1,62 @@
+package com.example.ballotblock.navigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.ballotblock.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class HomeScreen extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+    BottomNavigationView btmView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        btmView = findViewById(R.id.bottomNavigation);
+        Home home = new Home();
+        VoteNow vn = new VoteNow();
+        Candidate cad = new Candidate();
+        Main_Profile profile= new Main_Profile();
+        btmView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home_page:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, home).commit();
+                        Toast.makeText(getApplicationContext(), "Home page", Toast.LENGTH_SHORT).show();
+                        Log.d("TAG", "onNavigationItemSelected: Home Page");
+                        return true;
+                    case R.id.now_vote:
+                        Toast.makeText(getApplicationContext(), "Now Vote", Toast.LENGTH_SHORT).show();
+                        Log.d("TAG", "onNavigationItemSelected: Now Vote");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, vn).commit();
+                        return true;
+                    case  R.id.Candidate:
+                        Toast.makeText(getApplicationContext(), "Candidate", Toast.LENGTH_SHORT).show();
+                        Log.d("TAG", "onNavigationItemSelected: Cadidate");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, cad).commit();
+                        return true;
+                    case R.id.Profile:
+                        Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_SHORT).show();
+                        Log.d("TAG", "onNavigationItemSelected: Profile");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, profile).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/ballotblock/navigation/VoteNow.java
@@ -0,0 +1,22 @@
+package com.example.ballotblock.navigation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.ballotblock.R;
+
+public class VoteNow extends Fragment {
+    public VoteNow(){}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.votenowview, container, false);
+    }
+}
