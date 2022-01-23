d1d83c6a095d8ec9d284ae53e53a632763b568c5
abd-ashraf <abdaman7x@gmail.com>
2022-01-24 02:09:24 +0500

    FAQ toBeDone

---
+++ app/src/main/java/com/example/ballotblock/navigation/FAQ.java
@@ -0,0 +1,16 @@
+package com.example.ballotblock.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ballotblock.R;
+
+public class FAQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
